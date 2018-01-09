7c8350d723fa1ceaf4900f81e9ed4c454c9c3f6e
NotMichaelChen <mzc12345@gmail.com>
2018-01-09 15:07:49 -0500

    Finished implementing prefix opcodes

--- src/cpu_prefix.c
+++ src/cpu_prefix.c
@@ -4,6 +4,8 @@
 #include "memory.h"
 #include "bitmanip.h"
 
+//TODO: Refactor all bit shift operations into one function with different arguments
+
 //Decodes the next byte of the 2-byte instruction
 void prefix_decode(unsigned char instr, struct reg_type *reg) {
     unsigned char instr_type = get_bits(instr, 0, 1);
@@ -20,16 +22,22 @@ void prefix_decode(unsigned char instr, struct reg_type *reg) {
                 prefix_rotate_circular_right(instr, reg);
                 break;
             case 2:
+                prefix_rotate_left(instr, reg);
                 break;
             case 3:
+                prefix_rotate_right(instr, reg);
                 break;
             case 4:
+                prefix_shift_left_arith(instr, reg);
                 break;
             case 5:
+                prefix_shift_right_arith(instr, reg);
                 break;
             case 6:
+                prefix_swap(instr, reg);
                 break;
             case 7:
+                prefix_shift_right_logic(instr, reg);
                 break;
         }
 
@@ -62,7 +70,7 @@ void prefix_rotate_circular_left(unsigned char instr, struct reg_type *reg) {
     reset_flag('N', reg);
     reset_flag('H', reg);
     //Assign the Carry flag the old bit (which is the leftmost one)
-    *src_reg >> 7 ? set_flag('C', reg) : reset_flag('C', reg);
+    (*src_reg >> 7) ? set_flag('C', reg) : reset_flag('C', reg);
 
     *src_reg = result;
 }
@@ -74,6 +82,115 @@ void prefix_rotate_circular_right(unsigned char instr, struct reg_type *reg) {
     unsigned char *src_reg = decode_three_bit(reg_num, reg);
 
     unsigned char result = (*src_reg >> 1) | (*src_reg << 7);
+
+    //Assign flags before assigning the final result
+    set_flag_Z(result, reg);
+    reset_flag('N', reg);
+    reset_flag('H', reg);
+    //Assign the Carry flag the old bit (which is the rightmost one)
+    (*src_reg & 1) ? set_flag('C', reg) : reset_flag('C', reg);
+
+    *src_reg = result;
+}
+
+//Rotates the given register left. Incoming bit comes from carry, outgoing bit
+//goes to carry
+//0b0001 0xxx
+void prefix_rotate_left(unsigned char instr, struct reg_type *reg) {
+    unsigned char reg_num = get_bits(instr, 5, 7);
+    unsigned char *src_reg = decode_three_bit(reg_num, reg);
+
+    unsigned char result = (*src_reg << 1) | (get_flag('C', reg) >> 7);
+
+    //Assign flags before assigning the final result
+    set_flag_Z(result, reg);
+    reset_flag('N', reg);
+    reset_flag('H', reg);
+    //Assign the Carry flag the old bit (which is the leftmost one)
+    (*src_reg >> 7) ? set_flag('C', reg) : reset_flag('C', reg);
+
+    *src_reg = result;
+}
+
+//Rotates the given register right. Incoming bit comes from carry, outgoing bit
+//goes to carry
+//0b0001 1xxx
+void prefix_rotate_right(unsigned char instr, struct reg_type *reg) {
+    unsigned char reg_num = get_bits(instr, 5, 7);
+    unsigned char *src_reg = decode_three_bit(reg_num, reg);
+
+    unsigned char result = (*src_reg >> 1) | (get_flag('C', reg) << 7);
+
+    //Assign flags before assigning the final result
+    set_flag_Z(result, reg);
+    reset_flag('N', reg);
+    reset_flag('H', reg);
+    //Assign the Carry flag the old bit (which is the rightmost one)
+    (*src_reg & 1) ? set_flag('C', reg) : reset_flag('C', reg);
+
+    *src_reg = result;
+}
+
+//Shifts the given register left. Incoming bit is zero, outgoing bit goes to
+//carry
+//0b0010 0xxx
+void prefix_shift_left_arith(unsigned char instr, struct reg_type *reg) {
+    unsigned char reg_num = get_bits(instr, 5, 7);
+    unsigned char *src_reg = decode_three_bit(reg_num, reg);
+
+    unsigned char result = *src_reg << 1;
+
+    //Assign flags before assigning the final result
+    set_flag_Z(result, reg);
+    reset_flag('N', reg);
+    reset_flag('H', reg);
+    //Assign the Carry flag the old bit (which is the leftmost one)
+    (*src_reg >> 7) ? set_flag('C', reg) : reset_flag('C', reg);
+
+    *src_reg = result;
+}
+
+//Shifts the given register right. Bit 7 retains original value, bit 0 goes to
+//carry
+//0b0010 1xxx
+void prefix_shift_right_arith(unsigned char instr, struct reg_type *reg) {
+    unsigned char reg_num = get_bits(instr, 5, 7);
+    unsigned char *src_reg = decode_three_bit(reg_num, reg);
+
+    unsigned char result = (*src_reg >> 1) | (*src_reg & (1 << 7));
+
+    //Assign flags before assigning the final result
+    set_flag_Z(result, reg);
+    reset_flag('N', reg);
+    reset_flag('H', reg);
+    //Assign the Carry flag the old bit (which is the rightmost one)
+    (*src_reg & 1) ? set_flag('C', reg) : reset_flag('C', reg);
+
+    *src_reg = result;
+}
+
+//Swaps the upper and lower nibbles of the given register
+//0b0011 0xxx
+void prefix_swap(unsigned char instr, struct reg_type *reg) {
+    unsigned char reg_num = get_bits(instr, 5, 7);
+    unsigned char *src_reg = decode_three_bit(reg_num, reg);
+
+    *src_reg = (*src_reg << 4) | (*src_reg >> 4);
+
+    set_flag_Z(*src_reg, reg);
+    reset_flag('N', reg);
+    reset_flag('H', reg);
+    reset_flag('C', reg);
+}
+
+//Shifts the given register right. Incoming bit is zero, outgoing bit goes to
+//carry
+//0b0011 1xxx
+void prefix_shift_right_logic(unsigned char instr, struct reg_type *reg) {
+    unsigned char reg_num = get_bits(instr, 5, 7);
+    unsigned char *src_reg = decode_three_bit(reg_num, reg);
+
+    unsigned char result = *src_reg >> 1;
 
     //Assign flags before assigning the final result
     set_flag_Z(result, reg);
--- src/cpu_prefix.h
+++ src/cpu_prefix.h
@@ -7,6 +7,13 @@ void prefix_decode(unsigned char instr, struct reg_type *reg);
 
 void prefix_rotate_circular_left(unsigned char instr, struct reg_type *reg);
 void prefix_rotate_circular_right(unsigned char instr, struct reg_type *reg);
+void prefix_rotate_left(unsigned char instr, struct reg_type *reg);
+void prefix_rotate_right(unsigned char instr, struct reg_type *reg);
+void prefix_shift_left_arith(unsigned char instr, struct reg_type *reg);
+void prefix_shift_right_arith(unsigned char instr, struct reg_type *reg);
+void prefix_swap(unsigned char instr, struct reg_type *reg);
+void prefix_shift_right_logic(unsigned char instr, struct reg_type *reg);
+
 
 void prefix_testbit(unsigned char instr, struct reg_type *reg);
 void prefix_resetbit(unsigned char instr, struct reg_type *reg);
